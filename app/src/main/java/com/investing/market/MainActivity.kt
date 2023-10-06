4d8942b1ab9ca3dc413264bc1b08b32e3d6e49e5
Vanita Rathod <vanita.rathod@inkeysolutions.com>
2023-10-06 17:45:03 +0530

    aaaa

--- app/src/main/java/com/investing/market/MainActivity.kt
+++ app/src/main/java/com/investing/market/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-
+//change
         setSupportActionBar(binding.toolbar)
 
 
